49b819189727735a95a03bbd872feb0e49d6c8b0
mvenditto <venditto.matteo@gmail.com>
2023-06-02 20:02:55 +0100

    Basic chat completion implementation

--- gpt4all-bindings/csharp/Gpt4All/Gpt4All.cs
+++ gpt4all-bindings/csharp/Gpt4All/Gpt4All.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Text;
 using Gpt4All.Bindings;
+using Gpt4All.Chat;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
@@ -9,25 +11,33 @@ public class Gpt4All : IGpt4AllModel
 {
     private readonly ILLModel _model;
     private readonly ILogger _logger;
+    private IPromptFormatter _promptFormatter;
 
     private const string ResponseErrorMessage =
         "The model reported an error during token generation error={ResponseError}";
 
     /// <inheritdoc/>
-    public IPromptFormatter? PromptFormatter { get; set; }
+    public IPromptFormatter? PromptFormatter
+    {
+        get => _promptFormatter;
+        set => _promptFormatter = value ?? new DefaultPromptFormatter();
+    }
 
     internal Gpt4All(ILLModel model, ILogger? logger = null)
     {
         _model = model;
         _logger = logger ?? NullLogger.Instance;
-        PromptFormatter = new DefaultPromptFormatter();
+        _promptFormatter = new DefaultPromptFormatter();
     }
 
     private string FormatPrompt(string prompt)
     {
-        if (PromptFormatter == null) return prompt;
+        return _promptFormatter.FormatPrompt(prompt);
+    }
 
-        return PromptFormatter.FormatPrompt(prompt);
+    private string FormatChatPrompt(IChatConversation chat)
+    {
+        return _promptFormatter.FormatChatPrompt(chat);
     }
 
     public Task<ITextPredictionResult> GetPredictionAsync(string text, PredictRequestOptions opts, CancellationToken cancellationToken = default)
@@ -42,10 +52,11 @@ public class Gpt4All : IGpt4AllModel
             var result = new TextPredictionResult();
             var context = opts.ToPromptContext();
             var prompt = FormatPrompt(text);
+            _logger.LogDebug("Final prompt: {FinalPrompt}", prompt);
 
             try
             {
-                _model.Prompt(prompt, context, responseCallback: e =>
+                var usage = _model.Prompt(prompt, context, responseCallback: e =>
                 {
                     if (e.IsError)
                     {
@@ -57,6 +68,8 @@ public class Gpt4All : IGpt4AllModel
                     result.Append(e.Response);
                     return true;
                 }, cancellationToken: cancellationToken);
+
+                result.Usage = usage.ToPredictionInfo();
             }
             catch (Exception e)
             {
@@ -86,8 +99,9 @@ public class Gpt4All : IGpt4AllModel
             {
                 var context = opts.ToPromptContext();
                 var prompt = FormatPrompt(text);
+                _logger.LogDebug("Final prompt: {FinalPrompt}", prompt);
 
-                _model.Prompt(prompt, context, responseCallback: e =>
+                var usage = _model.Prompt(prompt, context, responseCallback: e =>
                 {
                     if (e.IsError)
                     {
@@ -99,6 +113,8 @@ public class Gpt4All : IGpt4AllModel
                     result.Append(e.Response);
                     return true;
                 }, cancellationToken: cancellationToken);
+
+                result.Usage = usage.ToPredictionInfo();
             }
             catch (Exception e)
             {
@@ -128,5 +144,106 @@ public class Gpt4All : IGpt4AllModel
     {
         Dispose(true);
         GC.SuppressFinalize(this);
+    }
+
+    public IChatConversation CreateNewChat()
+    {
+        var chat = new ChatConversation();
+        return chat;
+    }
+
+    public async Task<ITextPredictionResult> GetMessageAsync(IChatConversation chat, PredictRequestOptions? requestOptions = null, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(chat);
+
+        var opts = requestOptions ?? PredictRequestOptions.Chat;
+
+        chat.Context.ApplyOptions(opts);
+
+        var finalPrompt = FormatChatPrompt(chat);
+
+        _logger.LogDebug("Final prompt: {FinalPrompt}", finalPrompt);
+
+        var result = await Task.Run(() =>
+        {
+            var result = new TextPredictionResult();
+
+            var usage = _model.Prompt(finalPrompt, chat.Context, responseCallback: e =>
+            {
+                if (e.IsError)
+                {
+                    result.Success = false;
+                    result.ErrorMessage = e.Response;
+                    return false;
+                }
+                result.Append(e.Response);
+                return true;
+            }, cancellationToken: cancellationToken);
+
+            result.Usage = usage.ToPredictionInfo();
+
+            return (ITextPredictionResult)result;
+        }, CancellationToken.None);
+
+        var assistantMessage = await result.GetPredictionAsync(cancellationToken);
+
+        chat.AddMessage(ChatRole.Assistant, assistantMessage);
+
+        return result;
+    }
+
+    public Task<ITextPredictionStreamingResult> GetStreamingMessageAsync(IChatConversation chat, PredictRequestOptions? requestOptions = null, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(chat);
+
+        var opts = requestOptions ?? PredictRequestOptions.Chat;
+
+        chat.Context.ApplyOptions(opts);
+
+        var finalPrompt = FormatChatPrompt(chat);
+
+        _logger.LogDebug("Final prompt: {FinalPrompt}", finalPrompt);
+
+        var result = new TextPredictionStreamingResult();
+
+        var assistantMessage = new StringBuilder();
+
+        _ = Task.Run(() =>
+        {
+            try
+            {
+                var usage = _model.Prompt(finalPrompt, chat.Context, responseCallback: e =>
+                {
+                    if (e.IsError)
+                    {
+                        result.Success = false;
+                        result.ErrorMessage = e.Response;
+                        return false;
+                    }
+                    result.Append(e.Response);
+                    assistantMessage.Append(e.Response);
+                    return true;
+                }, cancellationToken: cancellationToken);
+
+                result.Usage = usage.ToPredictionInfo();
+            }
+            finally
+            {
+                result.Complete();
+            }
+        }, CancellationToken.None).ContinueWith(_ =>
+        {
+            if (result.Success)
+            {
+                chat.AddMessage(ChatRole.Assistant, assistantMessage.ToString());
+            }
+        }, cancellationToken);
+
+        return Task.FromResult((ITextPredictionStreamingResult)result);
+    }
+
+    public void SetThreadCount(int threadCount)
+    {
+        _model.SetThreadCount(threadCount);
     }
 }
