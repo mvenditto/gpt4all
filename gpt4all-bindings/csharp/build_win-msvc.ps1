ba15284615bc66fea44c28bcf7862af1af126e74
mvenditto <venditto.matteo@gmail.com>
2024-04-29 17:14:46 +0200

    fix msvc build script not copying to 'native' folder

--- gpt4all-bindings/csharp/build_win-msvc.ps1
+++ gpt4all-bindings/csharp/build_win-msvc.ps1
@@ -1,6 +1,13 @@
-Remove-Item -Force -Recurse .\runtimes\win-x64\msvc -ErrorAction SilentlyContinue
-mkdir .\runtimes\win-x64\msvc\build | Out-Null
-cmake -G "Visual Studio 17 2022" -A X64 -S ..\..\gpt4all-backend -B .\runtimes\win-x64\msvc\build
-cmake --build .\runtimes\win-x64\msvc\build --parallel --config Release
-cp .\runtimes\win-x64\msvc\build\bin\Release\*.dll .\runtimes\win-x64
-mv .\runtimes\win-x64\llmodel.dll .\runtimes\win-x64\libllmodel.dll
+$ROOT_DIR = '.\runtimes\win-x64'
+$BUILD_DIR = '.\runtimes\win-x64\msvc\build'
+$LIBS_DIR = '.\runtimes\win-x64\native'
+
+Remove-Item -Force -Recurse $ROOT_DIR -ErrorAction SilentlyContinue | Out-Null
+mkdir $BUILD_DIR | Out-Null
+mkdir $LIBS_DIR  | Out-Null
+
+cmake -G "Visual Studio 17 2022" -DKOMPUTE_OPT_DISABLE_VULKAN_VERSION_CHECK=ON -DKOMPUTE_OPT_USE_BUILT_IN_VULKAN_HEADER=OFF -A X64 -S ..\..\gpt4all-backend -B $BUILD_DIR
+cmake --build $BUILD_DIR --parallel --config Release
+
+cp "$BUILD_DIR\bin\Release\*.dll" $LIBS_DIR
+mv "$LIBS_DIR\llmodel.dll" "$LIBS_DIR\libllmodel.dll"
